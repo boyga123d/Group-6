d167191251a946a20687d34dd96fcee3814dc868
boyga123d <72071157+boyga123d@users.noreply.github.com>
2021-06-25 10:37:12 +0700

    Add project files.

---
+++ 1811061153/1811061153/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1811061153
+{
+    public class Books
+    {
+        public int BookID { get; set; }
+        public int BookName { get; set; }
+        public int Page { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Start/RouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+public class RouteConfig
+{
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        routes.MapRoute(
+            name: "Student",
+            url: "students/{id}",
+            defaults: new { controller = "Student", action = "Index" }
+        );
+
+        routes.MapRoute(
+            name: "Default",
+            url: "{controller}/{action}/{id}",
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );
+    }
+}
